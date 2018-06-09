f47a49c7b8cd374b2aa479680ef853b74eee8343
unknown <clayton.weller@gmail.com>
2018-06-09 12:18:27 -0600

    Finished Question 2

--- question2.js
+++ question2.js
@@ -1,34 +1,71 @@
 const startArray = require("./arr.js")
 
-const doTheyAdd = (num1, num2, target) =>{
-    let sum = num1 + num2
-    if(sum === target) {
-        return true
-    } else {
-        return false
-    }
-}
-
 const sumsFromIndex = (arr, index)=>{
     let sumArray = arr.map(i => [[i,arr[index]], i + arr[index]])
     sumArray.splice(index, 1)
     return sumArray
 }
 
-test= [1,2,3,4,5,11]
-
-const createAllSums = (arr)=>{
-    let allSumsArray = arr.map((val, i) => [val, sumsFromIndex(arr, i)])
-    return allSumsArray
+const checkSumArray = (arr, value)=>{
+    let goodPairsArray = arr.filter(sum => sum[1] === value)
+    return goodPairsArray
 }
 
-const targetSumCheck = (arr, val)=>{
-    
-    
-
+const filterGoodSumsByIndex = (arr, value, index) =>{
+    let sumArray = sumsFromIndex(arr, index)
+    let goodPairsArray = checkSumArray(sumArray, value)
+    return goodPairsArray
 }
 
-let allSumsArray = createAllSums(test)
+const filterAllGoodSums = (arr, val) =>{
+    let i = 0
+    let checkedSums = []
+    while(i<arr.length){
+        let newSums = filterGoodSumsByIndex(arr, val, i)
+        checkedSums = checkedSums.concat(newSums)
+        i++
+    }
+    checkedSums = checkedSums.map(val => val[0])
+    return checkedSums
+}
+
+const areSumsIdenticle = (arr1, arr2)=>{
+    if( arr1[0] === arr2[0] && arr1[1] === arr2[1]){
+        return true
+    } else if(arr1[0] === arr2[1] && arr1[1] === arr2[0]) {
+        return true
+    } else { 
+        return false
+    }
+}
 
 
-console.log(allSumsArray[4][1])
+
+const removeDuplicateSums = (arr) =>{
+    let i = 0
+    let modifiedArr = arr.map(n=>n)
+    while(i<modifiedArr.length){
+        let j = i+1
+        let isDuplicate = false
+        while( j<modifiedArr.length && isDuplicate === false){
+            isDuplicate = areSumsIdenticle(modifiedArr[i], modifiedArr[j])
+            j++
+        }
+        if(isDuplicate){
+            modifiedArr.splice(i,1)
+        }else{
+            i++
+        }
+    }
+    return modifiedArr
+}
+
+
+
+const findUniqueSummingPairs = (arr, val)=>{
+    let checkedSums = filterAllGoodSums(arr, val)
+    let cleanedArray = removeDuplicateSums(checkedSums)
+    return cleanedArray 
+}
+
+console.log(findUniqueSummingPairs(startArray, 3))
